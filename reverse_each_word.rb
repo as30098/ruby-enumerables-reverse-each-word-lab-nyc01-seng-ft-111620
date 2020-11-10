6c4ccca3f630198ecf4ab68add0ba90b803d2be4
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-10 01:50:22 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  result = ""
+  new_array = []
+  string = string.split(' ')
+  string.each do |word| 
+      new_array << word.reverse
+      result = new_array.join('')
+  end 
+  result
+end 
+
